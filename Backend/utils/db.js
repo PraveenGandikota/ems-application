5427c60b80e3055f3228a6e9fa8d821f1b7d66dd
Praveen <praveengandikota2222@gmail.com>
2025-02-26 16:51:35 +0530

    Updated backend files and added .gitignore

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,18 +1,42 @@
+// import mysql from 'mysql2';
+
+// const con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '@Creta143',
+//   database: 'EMS_DATABASE'
+// });
+
+// con.connect((err) => {
+//   if (err) {
+//     console.error('Connection Error:', err.message)
+//   } else {
+//     console.log("Connected to EMS_DATABASE");
+//   }
+// });
+
+// export default con;
+
+
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '@Creta143',
-  database: 'EMS_DATABASE'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 con.connect((err) => {
   if (err) {
-    console.error('Connection Error:', err.message)
+    console.error('Connection Error:', err.message);
   } else {
-    console.log("Connected to EMS_DATABASE");
+    console.log("Connected to Clever Cloud MySQL Database");
   }
-});
+});  
 
 export default con;
